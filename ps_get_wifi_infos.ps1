e93da0757a066b022cf52b34147d9c9c82041567
antonseitz <aseitz@puckit.de>
2023-10-09 17:07:53 +0200

    Update ps_get_wifi_infos.ps1

--- ps_get_wifi_infos.ps1
+++ ps_get_wifi_infos.ps1
@@ -1,27 +1,28 @@
-#
-
-
 # aktive SSID auslesen und MAC festhalten
 
-$wifi_connection=$(netsh wlan show interfaces)
+"ACTIVE WLAN: "
+$wifi_connection_active=$(netsh wlan show interfaces)
+
 
 #$wifi_connection
 
 
-# ist aus MAC Router ablesbar?
-foreach ($line in $wifi_connection) {
+foreach ($line in $wifi_connection_active) {
 
+# Router MAC: 
 if($line.contains( "BSSID" )){
 
 $bssid=($line -split ":",2)[1]
 }
 
-
+# Profile:
 if($line.contains( "Profil" )){
 $profile=($line -split ":",2)[1]
 
 }
 
+
+# SSID:
 if($line.contains( " SSID" )){
 
 $ssid=($line -split ":",2)[1]
@@ -35,11 +36,71 @@ $ssid=($line -split ":",2)[1]
 "Profile: " + $profile
 
 # WLAN Passwortlänge tracken
+
 $profile =$profile.Trim()
 
-
-
 $profile_content = & "C:\Windows\System32\netsh.exe"  wlan show profiles  name="$profile"  key=clear
+# im Debugger wird trotz key=clear KEIN passwort angezeigt.  ?!?
+
+#$profile_content 
+
+foreach ($profile_line in $profile_content) {
+
+#$profile_line
+# Key suchen 
+if($profile_line.contains( "inhalt" )){
+$passwd=($profile_line -split ":",2)[1]
+
+}
+}
+
+"PWD: " + $passwd
+
+
+
+
+"ACTIVE WLAN END "
+"---------------"
+
+"Other WLANs: "
+
+$wifi_connections_all=$(netsh wlan show profiles)
+
+
+
+
+foreach ($wifi in $wifi_connections_all) {
+
+if($wifi.contains( "Benutzer " )){
+
+$profile_name=($wifi -split ":",2)[1].Trim()
+
+$profile_content = & "C:\Windows\System32\netsh.exe"  wlan show profiles  name="$profile_name"  key=clear
+
+
+
+foreach ($line in $profile_content) {
+
+
+
+if($line.contains( " SSID" )){
+
+$ssid=($line -split ":",2)[1]
+}
+
+
+}
+
+
+"SSID:" + $ssid
+"Profile: " + $profile_name
+
+# WLAN Passwortlänge tracken
+#$profile =$profile.Trim()
+
+
+
+#$profile_content = & "C:\Windows\System32\netsh.exe"  wlan show profiles  name="$profile"  key=clear
 # im Debugger wird trotz key=clear KEIN passwort angezeigt. 
 
 #$profile_content 
@@ -54,6 +115,19 @@ $passwd=($profile_line -split ":",2)[1]
 "PWD: " + $passwd
 
 
+
+
+
+"---"
+
+
+
+
+
+
+
+}
+}
 
 "END"
 
