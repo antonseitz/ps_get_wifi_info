c69b0d443aafa865a9803a9e8fc6cfb77ec9a4c0
antonseitz <aseitz@puckit.de>
2023-10-09 14:38:31 +0200

    Update ps_get_wifi_infos.ps1

--- ps_get_wifi_infos.ps1
+++ ps_get_wifi_infos.ps1
@@ -3,14 +3,55 @@
 
 # aktive SSID auslesen und MAC festhalten
 
-netsh wlan show interfaces
+$wifi_connection=$(netsh wlan show interfaces)
 
+$wifi_connection
 
 
 # ist aus MAC Router ablesbar?
+foreach ($line in $wifi_connection) {
 
+if($line.contains( "BSSID" )){
+
+$bssid=($line -split ":",2)[1]
+}
+
+
+if($line.contains( "Profil" )){
+$profile=($line -split ":",2)[1]
+
+}
+
+if($line.contains( "SSID" )){
+
+$ssid=($line -split ":",2)[1]
+}
+
+
+}
+
+"RouterMAC:" + $bssid
+"SSID:" + $ssid
+"Profile: " + $profile
 
 # WLAN PasswortlÃ¤nge tracken
+$profile =$profile.Trim()
+
+
+
+$profile_content = & "C:\Windows\System32\netsh.exe"  wlan show profiles  name="$profile"  key=clear
+# im Debugger wird trotz key=clear KEIN passwort angezeigt. 
+
+$profile_content 
+
+foreach ($profile_line in $profile_content) {
+$profile_line
+if($profile_line.contains( "Schlüsselinhalt" )){
+$pwd=($profile_line -split ":",2)[1]
+
+}}
+
+"PWD: " + $pwd
 
 
 
